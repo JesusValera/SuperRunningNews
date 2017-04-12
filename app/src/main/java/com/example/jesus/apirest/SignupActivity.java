c96664d8fdf43e4db007bc4d71b5f3c8c18e24f6
Cristian Buitrago <kratorxiii@gmail.com>
2017-04-12 14:04:23 +0200

    Login y Registro realizados

--- app/src/main/java/com/example/jesus/apirest/SignupActivity.java
+++ app/src/main/java/com/example/jesus/apirest/SignupActivity.java
@@ -46,7 +46,6 @@ public class SignupActivity extends AppCompatActivity {
     private DatabaseReference ref;
     private FirebaseStorage storage;
 
-
     private ArrayList<Usuario> usuarios;
 
     private static final int SELECT_FILE = 1;
